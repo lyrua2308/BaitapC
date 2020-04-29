f04e4cb35db16886e2caeeb171ca1a7bf0b5dab3
lyrua2308 <64417448+lyrua2308@users.noreply.github.com>
2020-04-29 17:10:05 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,101 +1,115 @@
 ﻿using System;
 
-namespace Bai2_chuong3_part2
+namespace Bai1_Chuong3_Part2
 {
-    class tiendien
+    class tamgiac
     {
-        protected string hoten;
-        protected string diachi;
-        protected int congtothangtrc;
-        protected int congtothangnay;
-        protected int dadung;
-        protected int tien;
-        public tiendien()
+        protected int a, b, c;
+        protected double dientich;
+        public tamgiac()
         {
-            hoten = " ";
-            diachi = " ";
-            congtothangtrc = congtothangnay = 0;
-                
-        }    
-        public tiendien(string hoten, string diachi, int trc , int nay )
-        {
-            this.hoten = hoten;
-            this.diachi = diachi;
-            this.congtothangtrc = trc;
-            this.congtothangnay = nay;
+            a = b = c = 0;
         }
+        public tamgiac(int a,int b,int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
         public virtual void nhap()
         {
-            Console.WriteLine("nhap ho ten cua chu ho ");
-            hoten = Console.ReadLine();
-            Console.WriteLine("nhap dia chi cua chu ho ");
-            diachi = Console.ReadLine();
-            Console.WriteLine("nhap so cong to thang truoc ");
-            congtothangtrc = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("nhap so cong to thang nay  ");
-            congtothangnay = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("nhap canh thu nhat ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("nhap canh thu hai ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("nhap canh thu ba ");
+            c = Convert.ToInt32(Console.ReadLine());
         }
-        public double sodiendadung()
+         public virtual double tinhdientich()
         {
-            dadung = congtothangnay - congtothangtrc;
-            return dadung;
-        }
-        public double tinhtien()
-        {
-            tien = dadung * 500;
-            return tien;
+            double p = (a + b + c) * 0.5;
+            dientich = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return dientich;
         }
          public virtual void xuat()
         {
-            Console.WriteLine("chu ho {0} \t dia chi {1} \t tong so dien da dung {2} \t so tien thang nay {3}", hoten, diachi, dadung, tien);
+            Console.WriteLine("thong tin tam giac vua nhap la \n cac canh lan luot la: {0} , {1} ,{2}  \n co dien tich la {3}", a, b, c, dientich);
         }
+
     }
-    class tiendienmoi:tiendien
+    class TuDien:tamgiac
     {
-        protected int dinhmuc;
+        private double chieucao;
+        private double thetich;
         public override void nhap()
         {
             base.nhap();
-            Console.WriteLine("nhap dinh muc 500 hoac 600  ");
-            dinhmuc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("nhap vao chieu cao ");
+            chieucao = Convert.ToDouble(Console.ReadLine());
 
         }
-        public double tinhtien()
+        public double tinhthetich()
         {
-            tien = dadung * dinhmuc; 
-            return tien;
+            thetich = (dientich  * chieucao)/3;
+            return thetich;
         }
         public override void xuat()
         {
             base.xuat();
+            Console.WriteLine("the tich cua tu dien la " + thetich);
         }
+    }
+    class danhsachtamgiac
+    {
+        public int n;
+        public tamgiac[] DS;
+        public void nhap()
+        {
+            Console.WriteLine("nhap so luong phan tu  ");
+            n = Int32.Parse(Console.ReadLine());
+            DS = new tamgiac[n];
+            for (int i = 0; i < n; i++)
+            {
+                DS[i] = new tamgiac();
+                DS[i].nhap();
+            }
+        }
+        public void xuat()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                DS[i].tinhdientich();
+                DS[i].xuat();
 
-
+            }
+        }    
 
     }
     class Program
     {
         static void Main(string[] args)
         {
-            //tiendien td1;
-            //td1 = new tiendien();
-            //td1.nhap();
-            //td1.sodiendadung();
-            //td1.tinhtien();
-            //td1.xuat();
+            //tamgiac tg1;
+            //tg1 = new tamgiac(3, 4, 5);
+            //////tg1 = new tamgiac();
+            ////tg1.nhap();
+            //tg1.tinhdientich();
+            //tg1.tinhdientich();
+            //tg1.xuat();
 
-            //tiendien td2;
-            //td2 = new tiendien("nguyen thi ly ", "lam dien ", 2000, 3000);
-            //td2.sodiendadung();
-            //td2.tinhtien();
-            //td2.xuat();
-            tiendienmoi tdm;
-            tdm = new tiendienmoi();
-            tdm.nhap();
-            tdm.sodiendadung();
-            tdm.tinhtien();
-            tdm.xuat();
-            Console.Read();
+            //TuDien td1;
+            //td1 = new TuDien();
+            //td1.nhap();
+            //td1.tinhdientich();
+            //td1.tinhthetich();
+            //td1.xuat();
+            danhsachtamgiac ds1;
+            ds1 = new danhsachtamgiac();
+            ds1.nhap();
+            ds1.xuat();
+
+            Console.ReadLine();
         }
     }
 }
